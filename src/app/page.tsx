0b42e1c5e8928a5de4345e1c0476e6efa2478635
charliecool023 <charlesonah023@gmail.com>
2025-01-20 12:35:52 +0000

    width changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Header from '@/components/header';
+import Header from "@/components/header";
 import Hero from '@/components/hero';
 import Features from '@/components/features';
 import Pricing from '@/components/pricing';
@@ -6,18 +6,20 @@ import Faq from '@/components/faq';
 import Testimnials from '@/components/testimonials';
 import Download from '@/components/download';
 import Footer from '@/components/footer';
+import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <Header/>
-      <Hero />
-      <Features />
-      <Pricing />
-      <Faq />
-      <Testimnials />
-      <Download />
-      <Footer />
+    <div className="flex flex-col w-full">
+        <Header />
+        <Hero />
+        <Features />
+        <Pricing />
+        <Faq />
+        <Testimnials />
+        <Download />
+        <Footer />
     </div>
   );
 }
+
